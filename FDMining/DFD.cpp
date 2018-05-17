a4451f599d8538f8e63cb86cfe2a7f5fb6cca46e
ZhAnGeek <631084091@qq.com>
2018-05-18 07:36:41 +0800

    ''

--- FDMining/DFD.cpp
+++ FDMining/DFD.cpp
@@ -2,10 +2,93 @@
 
 using namespace std;
 
+DFDNode * DFDNode::RHS = nullptr;
+DFDTable DFDNode::Category;
+void DFDNode::getFromCategory()
+{
+	for (auto item : Category) {
+		if ((*this) >> item.second) {
+			if (item.second->isDependency()) {
+				this->type = Dependency;
+				return;
+			}
+		}
+		else if ((*item.second) >> this) {
+			if (item.second->isNonDependency()) {
+				this->type = NonDependency;
+				return;
+			}
+		}
+	}
+}
+
+bool DFDNode::operator>>(const DFDNode* other)
+{
+	int tsize = this->key.size(),osize = other->key.size();
+	if (tsize < osize)return false;
+	int t = 0, o = 0;
+	while (t < tsize && o < osize) {
+		if (this->key[t] == other->key[o])t++, o++;
+		else if (this->key[t] < other->key[o])t++;
+		else {
+			return false;
+		}
+	}
+	if (o >= osize) return true;
+
+	return false;
+}
+
 void DFD(RTable & r, FDSet & FDs) {
-    //如果要将算法函数改成算法类，也行
-    FDs = {
-        FDUnit({ 1 }, 3),
-        FDUnit({ 2, 5}, 1)
-    };
+
+	vector<RTable::iterator> DelWaitingList;
+	DFDNodeList RHSList;
+	auto beg = r.begin();
+	auto end = r.end();
+	int  rSize = r.size();
+
+	for (auto A = beg; A != end; A++) {
+			int key = A - beg;
+			auto column = (*A);
+			auto column_unique = set<string>(column.begin(), column.end());
+			if (column.size() == column_unique.size()) {
+				for (int i = 0; i < rSize; i++) FDs.push_back(FDUnit({ key }, i));
+				DelWaitingList.push_back(A);
+			}
+			else {
+				RHSList.push_back(new DFDNode({ key }));
+			}
+	}
+
+	for (auto DelColumn : DelWaitingList) {
+		r.erase(DelColumn);
+	}
+
+	for (auto RHS : RHSList) {
+		FDSet getLHS = findLHSs(RHS,RHSList,r);
+		FDs.insert(FDs.end(), getLHS.begin(), getLHS.end());
+	}
+}
+
+FDSet findLHSs(DFDNode *RHS,DFDNodeList RShip,RTable &r) {
+	FDSet result;
+	auto RHSPos = find(RShip.begin(), RShip.end(), RHS);
+	RShip.erase(RHSPos);
+	auto seeds = RShip;
+	DFDNode::RHS = RHS;
+	while (!seeds.empty()) {
+		auto node = seeds[0];
+		while (node) {
+			if (node->isVisited()) {
+
+			}
+			else {
+				node->getFromCategory();
+				if (!node->isVisited()) {
+					node->computePartitions();
+				}
+			}
+		}
+	}
+	return result;
 }
--- FDMining/DFD.h
+++ FDMining/DFD.h
@@ -1,5 +1,70 @@
 #pragma once
 
 #include "FDMining.h"
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <set>
+enum DFDNodeType {
+	UnVisited = 0,
+	Dependency = 1, MinimalDependency = 2, CandidateDependency = 3,
+	NonDependency = 4, MaximalNonDependency = 5, CandidateNonDependency = 6
+};
+class DFDNode;
+typedef std::vector<int> DFDKey;
+typedef std::vector<DFDNode*> DFDNodeList;
+typedef std::unordered_map<DFDKey, DFDNode*> DFDTable;
+
+class DFDNode {
+private:
+	DFDKey key;
+	DFDNodeType type;
+public:
+	static DFDNode *RHS;
+	static DFDTable Category;
+	DFDNode(DFDKey key): key(key) {
+		type = UnVisited;
+	}
+
+	void getFromCategory();
+
+	bool operator>> (const DFDNode* other);
+
+	void computePartitions() {
+
+	}
+
+	DFDNodeList getAllSubsets() {
+
+	}
+
+	DFDNodeList getAllSupersets() {
+
+	}
+
+	bool isVisited() {
+		return type != UnVisited;
+	}
+
+	bool isCandidateDependency() {
+		return type == CandidateDependency;
+	}
+
+	bool isCandidateNonDependency() {
+		return type == CandidateNonDependency;
+	}
+
+	bool isDependency() {
+		return type == Dependency || type == CandidateDependency || type == MinimalDependency;
+	}
+
+	bool isNonDependency() {
+		return type == NonDependency || type == MaximalNonDependency || type == CandidateNonDependency;
+	}
+
+	bool isMinimal();
+	bool isMaximal();
+};
 
 void DFD(RTable & r, FDSet & FDs);
+FDSet findLHSs(DFDNode *RHS, DFDNodeList RShip, RTable &r);
